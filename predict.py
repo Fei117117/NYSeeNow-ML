a77d141f1d901d7f1cb0faab59ed5decac252c04
Fei117117 <katherinekao@icloud.com>
2023-07-18 17:10:03 +0100

    add attraction prediction endpoint

--- predict.py
+++ predict.py
@@ -106,5 +106,73 @@ def predict():
     return jsonify(response)
 
 
+@app.route('/AttractionPredict', methods=['POST'])
+def AttractionPredict():
+    # Get the data from the POST request.
+    data = request.get_json(force=True)
+
+    # Extract the name, lat_lon, day, and month from the data
+    name = data['name']
+    lat_lon = data['lat_lon']
+    hour = data['hour']
+    day = data['day']
+    month = data['month']
+
+    # Split the lat_lon into latitude and longitude
+    lat, lon = map(float, lat_lon.split(','))
+
+    # Get the nearest station id
+    station_number = get_nearest_station_id(lon, lat)
+    type = 'area'  # area or station
+
+    if type == 'area':
+        path = f'./SubwayData/{type}_busy/a_busy_model_{station_number}.pkl'
+    elif type == 'station':
+        path = f'./SubwayData/{type}_busy/s_busy_model_{station_number}.pkl'
+    else:
+        return jsonify({'error': 'Invalid type.'})
+
+    try:
+        # Load the model
+        with open(path, 'rb') as handle:
+            model = pickle.load(handle)
+    except FileNotFoundError:
+        return jsonify({'error': 'Model not found.'})
+
+    # Prepare the input for the model including hour, day, month, and other required parameters
+    model_input = pd.DataFrame({
+        'hour': [hour],
+        'day': [day],
+        'month': [month],
+        'temperature': [15.3],  # Replace with actual temperature value
+        'rain_fall': [2.6],  # Replace with actual rain_fall value
+        'snow_fall': [0.0],  # Replace with actual snow_fall value
+        'Clear': [0],
+        'Clouds': [0],
+        'Drizzle': [0],
+        'Fog': [0],
+        'Haze': [0],
+        'Mist': [0],
+        'Rain': [1],
+        'Smoke': [0],
+        'Snow': [0],
+        'Thunderstorm': [0]
+    })
+
+    # Make prediction using the loaded model and the input data
+    prediction = model.predict(model_input.values)
+
+    # Take the first value of the prediction
+    output = int(prediction[0])
+
+    # Create the response dictionary
+    response = {
+        'name': name,
+        'prediction': output
+    }
+
+    return jsonify(response)
+
+
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
